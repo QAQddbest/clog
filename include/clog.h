dc660508868c41435c1892f34e54ef54f624ab65
OliverDD <oliverdd@std.uestc.edu.cn>
2020-05-28 22:05:05 +0800

    Add the project into clib

--- include/clog.h
+++ include/clog.h
@@ -1,26 +1,26 @@
-#ifndef LOG_H
-#define LOG_H
+#ifndef C_LOG_H
+#define C_LOG_H
 
 #include <stdio.h>
 #include <stdarg.h>
 
-typedef void (*log_LockFn)(void *mutex, int lock);
+typedef void (*CLogLockFn)(void *mutex, int lock);
 
 enum { LOG_TRACE, LOG_DEBUG, LOG_INFO, LOG_WARN, LOG_ERROR, LOG_FATAL };
 
-#define log_trace(...) log_CLog(LOG_TRACE, __FILE__, __LINE__, __VA_ARGS__)
-#define log_debug(...) log_CLog(LOG_DEBUG, __FILE__, __LINE__, __VA_ARGS__)
-#define log_info(...)  log_CLog(LOG_INFO,  __FILE__, __LINE__, __VA_ARGS__)
-#define log_warn(...)  log_CLog(LOG_WARN,  __FILE__, __LINE__, __VA_ARGS__)
-#define log_error(...) log_CLog(LOG_ERROR, __FILE__, __LINE__, __VA_ARGS__)
-#define log_fatal(...) log_CLog(LOG_FATAL, __FILE__, __LINE__, __VA_ARGS__)
+#define log_trace(...) c_clog_CLog(LOG_TRACE, __FILE__, __LINE__, __VA_ARGS__)
+#define log_debug(...) c_clog_CLog(LOG_DEBUG, __FILE__, __LINE__, __VA_ARGS__)
+#define log_info(...)  c_clog_CLog(LOG_INFO,  __FILE__, __LINE__, __VA_ARGS__)
+#define log_warn(...)  c_clog_CLog(LOG_WARN,  __FILE__, __LINE__, __VA_ARGS__)
+#define log_error(...) c_clog_CLog(LOG_ERROR, __FILE__, __LINE__, __VA_ARGS__)
+#define log_fatal(...) c_clog_CLog(LOG_FATAL, __FILE__, __LINE__, __VA_ARGS__)
 
-void log_SetMutex(void *mutex);
-void log_SetLock(log_LockFn fn);
-void log_SetFp(FILE *fp);
-void log_SetLevel(int level);
-void log_SetQuiet(int enable);
+void c_clog_SetMutex(void *mutex);
+void c_clog_SetLock(CLogLockFn fn);
+void c_clog_SetFp(FILE *fp);
+void c_clog_SetLevel(int level);
+void c_clog_SetQuiet(int enable);
 
-void log_CLog(int level, const char *file, int line, const char *fmt, ...);
+void c_clog_CLog(int level, const char *file, int line, const char *fmt, ...);
 
 #endif
--- src/clog.c
+++ src/clog.c
@@ -6,7 +6,7 @@
 
 static struct {
     void *mutex;
-    log_LockFn lock;
+    CLogLockFn lock;
     FILE *fp;
     int level;
     int quiet;
@@ -17,6 +17,13 @@ static const char *g_levelNames[] = {
         "TRACE", "DEBUG", "INFO", "WARN", "ERROR", "FATAL"
 };
 
+#ifdef C_CLOG_MUL_THREAD
+#define Lock() _Lock()
+#define Unlock() _Unlock()
+#else
+#define Lock(queue) ((void)0)
+#define Unlock(queue) ((void)0)
+#endif
 
 static void _Lock(void) {
     if (config.lock) {
@@ -32,38 +39,38 @@ static void _Unlock(void) {
 }
 
 
-void log_SetMutex(void *mutex) {
+void c_clog_SetMutex(void *mutex) {
     config.mutex = mutex;
 }
 
 
-void log_SetLock(log_LockFn fn) {
+void c_clog_SetLock(CLogLockFn fn) {
     config.lock = fn;
 }
 
 
-void log_SetFp(FILE *fp) {
+void c_clog_SetFp(FILE *fp) {
     config.fp = fp;
 }
 
 
-void log_SetLevel(int level) {
+void c_clog_SetLevel(int level) {
     config.level = level;
 }
 
 
-void log_SetQuiet(int enable) {
+void c_clog_SetQuiet(int enable) {
     config.quiet = enable ? 1 : 0;
 }
 
 
-void log_CLog(int level, const char *file, int line, const char *fmt, ...) {
+void c_clog_CLog(int level, const char *file, int line, const char *fmt, ...) {
     if (level < config.level) {
         return;
     }
 
     /* Acquire lock */
-    _Lock();
+    Lock();
 
     /* Get current time */
     time_t t = time(NULL);
@@ -96,5 +103,8 @@ void log_CLog(int level, const char *file, int line, const char *fmt, ...) {
     }
 
     /* Release lock */
-    _Unlock();
+    Unlock();
 }
+
+#undef Lock
+#undef Unlock
